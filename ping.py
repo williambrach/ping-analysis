34597873630641ac14ffba22be52f429d04b7b28
ramang22 <wibrach@gmail.com>
2021-02-03 19:32:02 +0100

    refactor

--- ping.py
+++ ping.py
@@ -1,5 +1,5 @@
-import platform    # For getting the operating system name
-import subprocess  # For executing a shell command
+import platform    
+import subprocess  
 from subprocess import Popen, PIPE
 import datetime
 import sys
@@ -10,9 +10,9 @@ import time
 addr = sys.argv[1]
 param = '-n' if platform.system().lower()=='windows' else '-c'
 command = ['ping', param, '1', "google.sk"]
-with open(f'{addr}_ping.csv', mode='w') as employee_file:
-    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    employee_writer.writerow(["date", "time", "ms"])
+with open(f'{addr}_ping.csv', mode='w') as ping_file:
+    excel_writer = csv.writer(ping_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    excel_writer.writerow(["date", "time", "ms"])
     while 1:
         p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
         output, err = p.communicate(b"input data that is passed to subprocess' stdin")
@@ -20,7 +20,7 @@ with open(f'{addr}_ping.csv', mode='w') as employee_file:
         x = datetime.datetime.now()
         time = x.strftime("%H:%M:%S")
         date = x.strftime("%d-%m-%Y")
-        employee_writer.writerow([date, time, ping])
+        excel_writer.writerow([date, time, ping])
         time.sleep(1)
 
 
